4bc2c8785a4f7a6223d8015afb7effbe875732d4
Siddhartha <sid@countvajhula.com>
2022-11-20 20:08:45 -0800

    try calling at root path explicitly

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ echo $(ls /)
 echo "... done."
 
 echo "Running money-in script..."
-python money-in.py
+python /money-in.py
 echo "... done."
 
 echo "Running money-out script..."
-python money-out.py
+python /money-out.py
 echo "... done."
 
