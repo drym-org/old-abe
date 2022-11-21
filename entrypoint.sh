6328d966f1db665cd7d84f063ac70103e1248d67
Siddhartha <sid@countvajhula.com>
2022-11-20 20:58:07 -0800

    create an issue with the outstanding balances computed

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,6 @@ echo "Running money-in script..."
 python /money-in.py
 echo "... done."
 
-echo "Running money-out script..."
-python /money-out.py
-echo "... done."
-
 echo "Committing updated transactions back to repo..."
 git config --global user.email "abe@drym.org"
 git config --global user.name "Old Abe"
@@ -26,3 +22,10 @@ git rebase origin/main
 git push origin main
 echo "... done."
 
+echo "Running money-out script..."
+python /money-out.py > issue-body.txt
+echo "... done."
+
+echo "Create an issue with outstanding balances..."
+gh issue create -F issue-body.txt -t "Outstanding Balances" -l outstanding-balances
+echo "... done."
