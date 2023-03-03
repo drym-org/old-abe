0fc0159c6e7b5616bb4da20f39b30e7cfd7d87a7
Old Abe <abe@drym.org>
2023-03-02 19:52:20 -0800

    strategically disable error handling to test job

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,11 +20,16 @@ echo "Committing updated transactions and attributions back to repo..."
 git config --global user.email "abe@drym.org"
 git config --global user.name "Old Abe"
 git add abe/transactions.txt abe/attributions.txt
+
+set +e
+
 git commit -m "Updated transactions and attributions"
 git fetch
 git rebase origin/main
 git push origin main
 echo "... done."
+
+set -e
 
 echo "Running money-out script..."
 echo balances=$(python /money-out.py) >> $GITHUB_OUTPUT
