bd869121c0d7115587ff9f52b5121ca627a619f7
apromessi <ariana@simplyinsured.com>
2023-04-20 00:57:26 -0700

    compute outstanding debts, create message

--- oldabe/money_out.py
+++ oldabe/money_out.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from .models import Transaction
+from .models import Transaction, Debt
 from datetime import datetime
 import csv
 import os
@@ -11,6 +11,7 @@ from decimal import Decimal, getcontext
 ABE_ROOT = 'abe'
 PAYOUTS_DIR = os.path.join(ABE_ROOT, 'payouts')
 TRANSACTIONS_FILE = os.path.join(ABE_ROOT, 'transactions.txt')
+DEBTS_FILE = os.path.join(ABE_ROOT, 'debts.txt')
 
 
 def read_transaction_amounts():
@@ -54,24 +55,61 @@ def compute_balances(owed: dict, paid: dict):
     return balances
 
 
-def compute debts():
-    pass
-
-
-def prepare_message(balances: dict):
+def prepare_balances_message(balances: dict):
     if not balances:
-        return "There are no outstanding balances."
+        return "There are no outstanding (payable) balances."
     balances_table = ""
     for name, balance in balances.items():
         balances_table += f"{name} | {balance:.2f}\n"
     message = f"""
-    The current outstanding balances are:
+    The current outstanding (payable) balances are:
 
     | Name | Balance |
     | ---- | --- |
     {balances_table}
 
     **Total** = {sum(balances.values()):.2f}
+    """
+    return "\r\n".join(line.strip() for line in message.split('\n')).strip()
+
+
+def read_outstanding_debt_amounts():
+    outstanding_debts = defaultdict(int)
+    with open(DEBTS_FILE) as f:
+        for row in csv.reader(f):
+            d = Debt(*row)
+            d.amount = Decimal(d.amount)
+            d.amount_paid = Decimal(d.amount_paid)
+            outstanding_amount = d.amount - d.amount_paid
+            outstanding_debts[d.email] += outstanding_amount
+    return outstanding_debts
+
+
+def prepare_debts_message(outstanding_debts: dict):
+    if not outstanding_debts:
+        return "There are no outstanding (unpayable) debts."
+    debts_table = ""
+    for name, debt in outstanding_debts.items():
+        debts_table += f"{name} | {debt:.2f}\n"
+    message = f"""
+    The current outstanding (unpayable) debts are:
+
+    | Name | Debt |
+    | ---- | --- |
+    {debts_table}
+
+    **Total** = {sum(outstanding_debts.values()):.2f}
+    """
+    return "\r\n".join(line.strip() for line in message.split('\n')).strip()
+
+
+def combined_message(balances_message, debts_message):
+    message = f"""
+    {balances_message}
+
+    ----------------------------
+
+    {debts_message}
     """
     return "\r\n".join(line.strip() for line in message.split('\n')).strip()
 
@@ -86,8 +124,10 @@ def main():
     owed = read_transaction_amounts()
     paid = read_payout_amounts()
     balances = compute_balances(owed, paid)
-    message = prepare_message(balances)
-    print(message)
+    balances_message = prepare_balances_message(balances)
+    outstanding_debts = read_outstanding_debt_amounts()
+    debts_message = prepare_debts_message(outstanding_debts)
+    print(balances_message)
 
 
 if __name__ == "__main__":
