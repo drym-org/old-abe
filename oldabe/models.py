de6ac760978b9cda1c6f3517283877043dc03914
Ariana Promessi <ariana@simplyinsured.com>
2023-04-20 08:34:21 +0530

    Set up debt model and related files (#13)

--- oldabe/models.py
+++ oldabe/models.py
@@ -12,6 +12,17 @@ class Transaction:
     created_at: datetime = field(default_factory=datetime.utcnow)
 
 
+class Debt:
+    email: str = None
+    amount: Decimal = None
+    # amount_paid is a running tally of how much of this debt has been paid
+    # in future will link to Transaction objects instead
+    amount_paid: Decimal = 0
+    payment_file: str = None
+    commit_hash: str = None
+    created_at: datetime = field(default_factory=datetime.utcnow)
+
+
 @dataclass
 class Payment:
     email: str = None
