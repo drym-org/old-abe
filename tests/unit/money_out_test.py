09791c5ed27baec6dd1d0cb0feaaa9c4cfff8896
Ariana Promessi <ariana@simplyinsured.com>
2023-04-22 07:18:37 +0530

    Tests for reporting outstanding balances (#18)

--- tests/unit/money_out_test.py
+++ tests/unit/money_out_test.py
@@ -1,6 +1,7 @@
 from oldabe.money_out import (
     compute_balances,
     prepare_balances_message,
+    prepare_debts_message,
 )
 from collections import defaultdict
 import pytest
@@ -57,7 +58,7 @@ class TestComputeBalances:
         assert result == expected_balances
 
 
-class TestPrepareMessage:
+class TestPrepareBalancesMessage:
     @pytest.mark.parametrize(
         "balances",
         [
@@ -75,3 +76,23 @@ class TestPrepareMessage:
         balances = {}
         result = prepare_balances_message(balances)
         assert result == "There are no outstanding (payable) balances."
+
+
+class TestPrepareDebtsMessage:
+    @pytest.mark.parametrize(
+        'debts',
+        [
+            {'abc@abe.com': 20, 'pqr@abe.com': 50},
+            {'abc@abe.com': 20},
+            {'abc@abe.com': 20, 'pqr@abe.com': 50, 'zab@abe.com': 10},
+        ],
+    )
+    def test_matrix(self, debts):
+        result = prepare_debts_message(debts)
+        for k in debts.keys():
+            assert k in result
+
+    def test_no_debts(self):
+        debts = {}
+        result = prepare_debts_message(debts)
+        assert result == "There are no outstanding (unpayable) debts."
