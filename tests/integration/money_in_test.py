25d6c49b3a11dad1d594f4f42661f7e87d713d0d
Siddhartha <sid@countvajhula.com>
2024-04-18 20:40:00 -0700

    a couple of starter integration tests

--- tests/integration/money_in_test.py
+++ tests/integration/money_in_test.py
@@ -1,4 +1,6 @@
 import pytest
+from datetime import datetime
+import time_machine
 from unittest.mock import patch
 from oldabe.money_in import (
     process_payments_and_record_updates,
@@ -7,8 +9,8 @@ import os
 
 
 @patch('oldabe.money_in.get_git_revision_short_hash', return_value='abcd123')
-def test_creates_transactions_on_payment(mock_git_rev, fs):
-    ff = fs.create_file("./abe/price.txt", contents="10")
+def test_no_transactions_if_no_payments(mock_git_rev, fs):
+    fs.create_file("./abe/price.txt", contents="10")
     fs.create_file("./abe/valuation.txt", contents="100000")
     fs.create_file("./abe/instruments.txt", contents="""
         old abe,1
@@ -19,13 +21,39 @@ def test_creates_transactions_on_payment(mock_git_rev, fs):
        jair,30
        ariana,20
        """)
-    # print(ff.name)
-    # print(ff.contents)
-    # print(ff.filesystem)
     with open('abe/price.txt') as f:
         assert f.read() == "10"
     process_payments_and_record_updates()
     with open('./abe/transactions.txt') as f:
-        assert 'sid' in f.read()
-        assert 'jair' in f.read()
-        assert 'ariana' in f.read()
+        assert f.read() == ""
+
+
+@time_machine.travel(datetime(1985, 10, 26, 1, 24), tick=False)
+@patch('oldabe.money_in.get_git_revision_short_hash', return_value='abcd123')
+def test_creates_transactions_on_payment(mock_git_rev, fs):
+    fs.create_file("./abe/price.txt", contents="10")
+    fs.create_file("./abe/valuation.txt", contents="100000")
+    fs.create_file("./abe/instruments.txt", contents="""
+        old abe,1
+        DIA,5
+        """)
+    fs.create_file("./abe/attributions.txt", contents="""
+       sid,50
+       jair,30
+       ariana,20
+       """)
+
+    fs.create_file("./abe/payments/1.txt",
+                   contents="sam,036eaf6,100,dummydate")
+    process_payments_and_record_updates()
+    with open('./abe/transactions.txt') as f:
+        # TODO: figure out why it's writing 3 decimal places
+        # and decide on handling
+        assert f.read() == (
+            "old abe,1.000,1.txt,abcd123,1985-10-26 01:24:00\n"
+            "DIA,5.000,1.txt,abcd123,1985-10-26 01:24:00\n"
+            "sid,47.000000,1.txt,abcd123,1985-10-26 01:24:00\n"
+            "jair,28.200000,1.txt,abcd123,1985-10-26 01:24:00\n"
+            "ariana,18.800000,1.txt,abcd123,1985-10-26 01:24:00\n"
+        )
+
