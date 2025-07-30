708ec76df7257434f98e10728f0318bd1af45359
Siddhartha <sid@countvajhula.com>
2025-07-29 20:56:30 -0700

    debug: print attributions total

--- oldabe/accounting.py
+++ oldabe/accounting.py
@@ -4,7 +4,9 @@ from fractions import Fraction
 def assert_attributions_normalized(attributions):
     """A complete set of attributions must be normalized, i.e. must add up to
     1. This raises an error if the provided attributions are not normalized."""
-    assert _get_attributions_total(attributions) == Fraction(1, 1)
+    total = _get_attributions_total(attributions)
+    print(total)
+    assert total == Fraction(1, 1)
 
 
 def _get_attributions_total(attributions):
