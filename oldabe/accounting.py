d210fb73b2b45d1155100159f68b4943838fff44
Siddhartha <sid@countvajhula.com>
2025-07-29 21:08:16 -0700

    remove print statement that was there for debugging

--- oldabe/accounting.py
+++ oldabe/accounting.py
@@ -4,9 +4,7 @@ from fractions import Fraction
 def assert_attributions_normalized(attributions):
     """A complete set of attributions must be normalized, i.e. must add up to
     1. This raises an error if the provided attributions are not normalized."""
-    total = _get_attributions_total(attributions)
-    print(total)
-    assert total == Fraction(1, 1)
+    assert _get_attributions_total(attributions) == Fraction(1, 1)
 
 
 def _get_attributions_total(attributions):
