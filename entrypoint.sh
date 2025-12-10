251b99c948ad5fcc7997c90c6477a8e0461015de
Siddhartha <sid@countvajhula.com>
2025-12-09 21:35:04 -0700

    Only commit after both money_in and money_out have run
    This ensures that we do not run into the problem where interrupted
    jobs end up trying to commit the same changes to accounting records
    more than once. Also, ensure that we do not attempt to run money_out
    if money_in fails. Finally, note a potential race condition and ways
    to handle it.

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,8 +17,35 @@ echo "Running ls /github/workspace..."
 echo $(ls /github/workspace)
 echo "... done."
 
+# TODO: note that we do not support multiple consecutive
+# ABE-related commits with jobs running concurrently,
+# as subsequent jobs would not have the up-to-date accounting
+# data that would be produced by earlier jobs that have
+# not yet finished running.
+# So either:
+#  (a) ensure that ABE-related jobs are always run after the previous
+#      one concludes.
+#  (b) employ a locking mechanism so that no two instances of Old Abe
+#      can run concurrently (better).
+
 echo "Running money_in script..."
 python -m oldabe.money_in.__main__
+
+# $? holds the exit status of the last executed command
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+echo "... done."
+
+echo "Running money_out script..."
+BALANCES_OUTPUT=$(python -m oldabe.money_out.__main__)
+
+# $? holds the exit status of the last executed command
+if [ $? -eq 0 ]; then
+    echo balances=$BALANCES_OUTPUT >> $GITHUB_OUTPUT
+else
+    exit 1
+fi
 echo "... done."
 
 # Note that running this locally would cause your global
@@ -32,15 +59,4 @@ git commit -m "Updated accounting records"
 git fetch
 git rebase origin/`git remote set-head origin -a | cut -d' ' -f4`
 git push origin `git remote set-head origin -a | cut -d' ' -f4`
-echo "... done."
-
-echo "Running money_out script..."
-BALANCES_OUTPUT=$(python -m oldabe.money_out.__main__)
-
-# $? holds the exit status of the last executed command
-if [ $? -eq 0 ]; then
-    echo balances=$BALANCES_OUTPUT >> $GITHUB_OUTPUT
-else
-    exit 1
-fi
 echo "... done."
